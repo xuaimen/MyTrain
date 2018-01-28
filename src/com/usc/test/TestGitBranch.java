ab4951bc9f37813b945b175b487e8253a4477b22
snake <709456276@qq.com>
2018-01-28 11:52:29 +0800

    test commit branch

---
+++ src/com/usc/test/TestGitBranch.java
@@ -0,0 +1,10 @@
+package com.usc.test;
+
+/**
+ * Created by xuqiang on 2018/1/28.
+ */
+public class TestGitBranch {
+    public static void main(String[] args) {
+        System.out.println("this is a git branch test");
+    }
+}
