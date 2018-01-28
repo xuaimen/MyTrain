c21f31b80e049bb82ba297f99ad1583d90a9067f
snake <709456276@qq.com>
2018-01-28 11:28:18 +0800

    idea commit

---
+++ src/com/usc/test/Test.java
@@ -0,0 +1,10 @@
+package com.usc.test;
+
+/**
+ * Created by xuqiang on 2018/1/28.
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.print("this is a git test!");
+    }
+}
